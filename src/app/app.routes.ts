01ec5f8632ed4fa5913be85315cea9eac53ff98b
Shahir Jalal <shahirjalalhazli@gmail.com>
2024-11-16 13:10:51 +0800

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { QuoteComponent } from './pages/quote/quote.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'quote', component: QuoteComponent },
+  { path: '**', redirectTo: '' }
+];
