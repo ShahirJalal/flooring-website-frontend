3174dcc11a128d4b8be9f2e6f3f83565443360d4
Shahir Jalal <shahirjalalhazli@gmail.com>
2024-11-17 23:32:51 +0800

    alternative

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://localhost:8080/api'
-    apiUrl: 'https://flooring-website-backend-68e142dd2901.herokuapp.com/api'
+    apiUrl: 'http://localhost:8080/api'
 };
