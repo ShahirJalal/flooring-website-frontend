b15eba7b5773bc2a51a08ce8913f5abd0165f176
Shahir Jalal <shahirjalalhazli@gmail.com>
2024-11-17 16:47:46 +0800

    Deployment update

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/frontend/browser'));  // Update path
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/frontend/browser/index.html'));  // Update path
 });
 
 // Start the app by listening on the default Heroku port
